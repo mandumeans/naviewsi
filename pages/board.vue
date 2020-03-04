c4d4303535526267e5293bb552ecfe5ce229674e
burningmagic <burningmagic@nexon.co.kr>
2020-03-04 18:50:58 +0900

    약간 리팩토링 승자구하는 로직 넣었음

--- pages/board.vue
+++ pages/board.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="board" class="board-row">
     <div class="board-row" v-for="(square_line_list, xIdx) in square_list" :key="square_line_list.id">
-      <button class="square" @click="changeButtonState(xIdx, yIdx)" v-for="(item, yIdx) in square_line_list" :key="item.id">
+      <button class="square" @click="changeButtonState($event, xIdx, yIdx)" v-for="(item, yIdx) in square_line_list" :key="item.id">
           {{ item.value }}
       </button>
     </div>
@@ -11,38 +11,13 @@
 <script>
 export default {
   name: 'board',
+  props: ['square_list', 'changeButtonState']
+  ,
   data() {
-    return {
-      square_list: [],
-      xIsNext: true
-    }
   },
   created: function(){
-    this.square_list = this.getSquareList();
   },
   methods: {
-    getSquareList: function(){
-        const BOARD_SIZE = 3;
-        const square_list = [];
-
-        for(let i = 0; i < BOARD_SIZE; i++) {
-          const square_line_list = [];
-            for(let j = 0; j < BOARD_SIZE; j++) {
-                const number = (BOARD_SIZE * i) + j;
-                square_line_list.push({
-                  number : number,
-                  value : ""
-                });
-            }
-            square_list.push(square_line_list);
-        }
-        return square_list;
-    },
-    changeButtonState: function(x, y){
-      var vm = this;
-      this.square_list[x][y].value = (this.xIsNext ? 'X' : 'O');
-      vm.xIsNext = !vm.xIsNext;
-    }
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="game">
         <div class="game-board">
-          <board/>
+          <board :square_list="square_list" :changeButtonState="changeButtonState"/>
         </div>
         <div class="game-info">
             <div>status</div>
@@ -21,11 +21,84 @@ export default {
   },
   data(){
     return {
+      square_list: [],
+      xIsNext: true
     }
   },
   created: function(){
+    this.square_list = this.getSquareList();
   },
   methods: {
+    calculateWinner: function(squares){
+      const lines = [
+          [[0,0], [0,1], [0,2]],
+          [[1,0], [1,1], [1,2]],
+          [[2,0], [2,1], [2,2]],
+          [[0,0], [1,0], [2,0]],
+          [[0,1], [1,1], [2,1]],
+          [[0,2], [1,2], [3,1]],
+          [[0,0], [1,1], [2,2]],
+          [[0,2], [1,1], [2,0]],
+      ];
+      let result = null;
+      
+      for (let i = 0; i < lines.length; i++){
+        const [ax, ay] = lines[i][0];
+        const [bx, by] = lines[i][1];
+        const [cx, cy] = lines[i][2];
+        if(squares[ax][ay] && squares[ax][ay] === squares[bx][by] && squares[ax][ay] === squares[cx][cy]) {
+            result = {
+                winnerLine: lines[i],
+                winner:squares[ax][ay],
+                isDraw:false
+            };
+        }
+      }
+
+      let isEmpty = false;
+      for(let i = 0; i < squares.length; i++){
+          if(squares[i][0].value == "" || squares[i][1].value == "" || squares[i][2].value == "") {
+              isEmpty = true;
+          }
+      }
+
+      //squares가 끝났는데 승패가 안갈렸으면 
+      if(isEmpty === false && result === null){
+          result = {
+              isDraw: true
+          }
+      }
+
+      return result;
+    },
+    
+    getSquareList: function(){
+        const BOARD_SIZE = 3;
+        const square_list = [];
+
+        for(let i = 0; i < BOARD_SIZE; i++) {
+          const square_line_list = [];
+            for(let j = 0; j < BOARD_SIZE; j++) {
+                const number = (BOARD_SIZE * i) + j;
+                square_line_list.push({
+                  number : number,
+                  value : ""
+                });
+            }
+            square_list.push(square_line_list);
+        }
+        return square_list;
+    },
+
+    changeButtonState: function(event, x, y){
+      if (event) event.preventDefault();
+      if (this.calculateWinner(this.square_list) || this.square_list[x][y].value != "") {
+        return;
+      }
+      var vm = this;
+      this.square_list[x][y].value = (this.xIsNext ? 'X' : 'O');
+      vm.xIsNext = !vm.xIsNext;
+    }
   }
 }
 </script>
